8ad7bfa79a54d268cab3e974fdee09758fc1972a
teesloane <tylersloane@gmail.com>
2016-09-10 13:41:01 -0400

    lol bufferLength.length DAMMIT

--- src/ui/oscilloscope.js
+++ src/ui/oscilloscope.js
@@ -10,48 +10,48 @@ export default class Oscilloscope extends UIObj {
     this.ctx.clearRect(0, 0, this.width, this.height);
 
     this.oscScope = actx.createAnalyser();
-    console.log(this.oscScope);
     this.oscScope.fftSize = 2048;
     this.bufferLength = this.oscScope.frequencyBinCount;
     this.dataArray = new Uint8Array(this.bufferLength);
-    this.oscScope.getByteTimeDomainData(this.dataArray); 
+    this.oscScope.getByteTimeDomainData(this.dataArray);
 
- }
+  }
 
- draw() {
-   // the animation loop
-   let drawVisual = requestAnimationFrame(this.draw.bind(this));
 
-   // ?
-   this.oscScope.getByteTimeDomainData(this.dataArray);
+  draw() {
+    // the animation loop
+    let drawVisual = requestAnimationFrame(this.draw.bind(this));
 
-   // the style of the osc.
-   this.ctx.fillStyle = 'rgb(200, 200, 200)';
-   this.ctx.fillRect(0, 0, this.width, this.height);
-   this.ctx.lineWidth = 2;
-   this.ctx.strokeStyle = 'rgb(0, 0, 0)';
+    // ?
+    this.oscScope.getByteTimeDomainData(this.dataArray);
 
-   this.ctx.beginPath();
+    // the style of the osc.
+    this.ctx.fillStyle = 'rgb(10, 200, 200)';
+    this.ctx.fillRect(0, 0, this.width, this.height);
+    this.ctx.lineWidth = 2;
+    this.ctx.strokeStyle = 'rgb(255, 255, 0)';
 
-  this.sliceWidth = this.width * 1.0 / this.bufferLenght;
-  this.sliceX = 0;
+    this.ctx.beginPath();
 
-   // draw the osc from moment to moment.
-   for (var i = 0; i < this.bufferLength.length; i++) {
-     let v = this.dataArray[i] / 128.0;
-     let sliceY = v * this.height/2;
+    this.sliceWidth = this.width * 1.0 / this.bufferLength;
+    this.sliceX = 0;
 
-     if (i === 0) {
-       this.ctx.moveTo(this.sliceX, sliceY);
-     } else {
-       this.ctx.lineTo(this.sliceX, sliceY);
-     }
+    // draw the osc from moment to moment.
+    for (var i = 0; i < this.bufferLength; i++) {
+      var v = this.dataArray[i] / 128.0;
+      var sliceY = v * this.height / 2;
 
-     this.sliceX += this.sliceWidth;
-   }
+      if (i === 0) {
+        this.ctx.moveTo(this.sliceX, sliceY);
+      } else {
+        this.ctx.lineTo(this.sliceX, sliceY);
+      }
 
-  this.ctx.lineTo(this.ctx.width, this.ctx.height/2);
-  this.ctx.stroke();
- }
+      this.sliceX += this.sliceWidth;
+    }
+
+    this.ctx.lineTo(this.ctx.width, this.ctx.height / 2);
+    this.ctx.stroke();
+  }
 
 }
