16d12dfd755697d52362da7c1d895d6f34e8b07c
bbales <ben.bales.dev@gmail.com>
2016-09-19 11:36:41 -0400

    adding note selector for basic sequencer

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import _ from 'lodash'
 // Local deps
 import './util/protos'
 import * as Knobs from './ui/knobs/knobs'
+import Knob from './ui/knob'
 import Canvas from './ui/canvas'
 import Voice from './audio/voice'
 import Sequencer from './audio/sequencer'
@@ -16,10 +17,11 @@ import Switch from './ui/switch'
 import Oscilloscope from './ui/oscilloscope'
 
 import Notes from './audio/notes'
+Notes.all
 
 // UI
 var canvas = new Canvas('canvas')
-var keys = new Keys(canvas).setPos(200, 500)
+    // var keys = new Keys(canvas).setPos(200, 500)
 
 // Audio
 var actx = new window.AudioContext()
@@ -33,7 +35,7 @@ var sequencer = new Sequencer().connect(v1).connect(v2).setBPM(190).play()
 sequencer.writeNote(Notes.note('c5'), 0)
 sequencer.writeNote(Notes.note('c4'), 1)
 sequencer.writeNote(Notes.note('c6'), 2)
-sequencer.writeNote(Notes.note('c5'), 3)
+sequencer.writeNote(Notes.note('cs5'), 3)
 sequencer.writeNote(Notes.note('c4'), 4)
 sequencer.writeNote(Notes.note('c6'), 5)
 sequencer.writeNote(Notes.note('c5'), 6)
@@ -43,8 +45,8 @@ sequencer.writeNote(Notes.note('c4'), 7)
 let oScope = new Oscilloscope(canvas, actx, [v1, v2]).setPos(600, 150)
 
 // Keys
-keys.attach(v1)
-keys.attach(v2)
+// keys.attach(v1)
+// keys.attach(v2)
 
 // OSC1
 var osc1_shape = new Knobs.ShapeKnob(canvas, v1).setPos(200, 150)
@@ -64,10 +66,56 @@ var osc2_enable = new Switch(canvas, 'OSC2 Enable').setPos(100, 235).change(v =>
 var colors = ['red', 'green', 'yellow', 'violet', 'orange'];
 for (var i = 0; i < 8; i++) {
     var led1 = new Led(canvas)
-    led1.setPos(200 + 55 * i, 50)
+    var led1_octave = new Knob(canvas, 'Step' + (i + 1) + ' Note').setPos(200 + 65 * i, 450).setRadius(10)
+
+    var led1_note = new Knob(canvas, 'Step' + (i + 1) + ' Octave').setPos(200 + 65 * i, 540).setRadius(10).change(value => {
+        console.log(Notes.note(value))
+        sequencer.writeNote(Notes.note(value, 5), index)
+    }).setSnaps([{
+        value: 'a',
+        text: 'a'
+    }, {
+        value: 'as',
+        text: 'a#'
+    }, {
+        value: 'b',
+        text: 'b'
+    }, {
+        value: 'c',
+        text: 'c'
+    }, {
+        value: 'cs',
+        text: 'c#'
+    }, {
+        value: 'd',
+        text: 'd'
+    }, {
+        value: 'ds',
+        text: 'd#'
+    }, {
+        value: 'e',
+        text: 'e'
+    }, {
+        value: 'f',
+        text: 'f'
+    }, {
+        value: 'fs',
+        text: 'f#'
+    }, {
+        value: 'g',
+        text: 'g'
+    }, {
+        value: 'gs',
+        text: 'g#'
+    }])
+    led1.setPos(200 + 65 * i, 400)
     led1.setColor(colors[i])
+
+    // Sandbox index
     let index = i
-    led1.check(function() {
+
+    // Assign check function
+    led1.check(() => {
         return sequencer._currentStep == index
     })
 }
